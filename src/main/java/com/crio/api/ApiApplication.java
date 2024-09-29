e7b80aa6d41bb18058529a05d3f3e802fcda8b4e
dacasfilipe <filiperdacas@gmail.com>
2024-09-28 21:52:36 -0300

    feat: fixed configs

--- src/main/java/com/crio/api/ApiApplication.java
+++ src/main/java/com/crio/api/ApiApplication.java
@@ -1,6 +1,5 @@
 package com.crio.api;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.out.println("username:"+dotenv.get("DB_USERNAME"));
-		System.out.println("password:"+dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
